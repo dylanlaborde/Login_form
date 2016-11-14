9b0d10d5114860cc4ea032cda2ff60879c664942
LABORDE <dylan4047@gmail.com>
2016-11-14 16:51:45 +0100

    add function to recup form and post to server

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+(function(){
+	"use strict"
+	var app = {
+		
+		init : function(){
+			app.watcher();
+		},
+
+		watcher : function(){
+			$('form').on("submit",app.recupForm);
+		},
+
+		recupForm : function(event){
+		  event.preventDefault();
+			 var data = $("form").serialize();
+
+/*autre methode pour recuperer data
+			var data = {
+				username:$("#username").val(),
+				password:$("#password").val(),
+			}
+*/
+			$.ajax({
+				type :"POST",
+				url :$("form").attr("action"),
+				data :data,
+				success:function(){
+					console.log(data)
+				},
+			})
+			
+		},
+	};
+
+	app.init()
+})();
